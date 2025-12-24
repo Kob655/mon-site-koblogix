11d61cf527477339684ba445f5fa642aa25af8b8
Koboyo <Koblogixofficiel@gmail.com>
2025-12-24 00:11:58 +0000

    Fix: vraies cles Firebase en dur

--- firebase.ts
+++ firebase.ts
@@ -1,20 +1,15 @@
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// REMPLACEZ CES VALEURS PAR CELLES DE VOTRE CONSOLE FIREBASE
+// Vos vraies clés Firebase (à récupérer sur console.firebase.google.com)
 const firebaseConfig = {
-  apiKey: " AIzaSyCVKVZZCw87xrLyWkF1uJaqzJ1v_ZPCDf4 ",
-  authDomain: "koblogix.firebaseapp.com ",
-  projectId: "koblogix ",
-  storageBucket: "koblogix.firebasestorage.app ",
-  messagingSenderId: "1059133656016 ",
+  apiKey: " AIzaSyCVKVZZCw87xrLyWkF1uJaqzJ1v_ZPCDf4",
+  authDomain: "koblogix.firebaseapp.com ,
+  projectId: "koblogix",
+  storageBucket: "koblogix.firebasestorage.app",
+  messagingSenderId: "1059133656016",
   appId: "1:1059133656016:web:684a56716bd086e6cc47d5"
 };
 
-// Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export de la base de données Firestore
 export const db = getFirestore(app);
-
